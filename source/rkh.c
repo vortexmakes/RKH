583af6c7ad407fde16ec7fc882c3e7cdf3f9e486
vortexleo <francuccilea@gmail.com>
2011-06-22 17:46:01 +0000

    Added member dynamic_ to RKHEVT_T structure to support automatic garbage collection. Added rkh_ae(), rkh_gc(), rkh_put_fifo(), rkh_put_lifo(), rkh_defer() and rkh_recall() functions to support dynamic and defer events.

--- source/rkh.c
+++ source/rkh.c
@@ -35,6 +35,7 @@
 
 #include "rkh.h"
 #include "rkhsm.h"
+#include "rkhassert.h"
 
 
 #define EXIT_LIST						0x0
@@ -110,13 +111,13 @@
 
 #define rkh_process_input( s, h, pe )									\
 																		\
-	(s)->prepro != NULL ? rkh_call_prepro(s,h,pe) : (*pe)
+	(s)->prepro != NULL ? rkh_call_prepro(s,h,pe) : (pe->e)
 
 #else
 
 #define rkh_process_input( s, h, pe )									\
 																		\
-	(*pe)
+	(pe->e)
 
 #endif
 
@@ -626,5 +627,133 @@ rkh_else( void )
 {
 	return RKH_GTRUE;
 }
+
+#endif
+
+
+#if RKH_EN_DYNAMIC_EVENT == 1
+
+RKHEVT_T *
+rkh_ae( rkhuint8 es, RKHE_T e )
+{
+    RKHEVT_T *evt;
+
+              /* find the pool index that fits the requested event size ... */
+    rkhuint8 idx = 0;
+    while( es > rkh_dyne_event_size( idx ) ) 
+	{
+        ++idx;
+									  /* cannot run out of registered pools */
+        rkhassert( idx < RKH_DYNE_NUM_POOLS, RKH_AE_RUNOUT_POOLS );
+    }
+
+    rkh_dyne_get( idx, evt );        		 /* get e -- platform-dependent */
+							             /* pool must not run out of events */
+    rkhassert( evt != NULL, RKH_AE_NOT_ALLOC );
+
+    evt->e = e;                                /* set signal for this event */
+
+	/* 
+	 * Store the dynamic attributes of the event:
+	 * the pool ID and the reference counter == 0
+	 */
+    evt->dynamic_ = (rkhuint8)( ( idx + 1 ) << 6 );
+    return e;	
+}
+
+
+void 
+rkh_gc( RKHEVT_T *evt )
+{
+    if( evt->dynamic_ != 0 )                      /* is it a dynamic event? */
+	{
+        rkh_enter_critical();
+
+        if( ( evt->dynamic_ & 0x3F ) > 1 )      /* isn't this the last ref? */
+		{
+            --((RKHEVT_T*)e)->dynamic_;   /*decrement the reference counter */
+            rkh_exit_critical();
+        }
+        else        /* this is the last reference to this event, recycle it */
+		{
+                                                      /* cannot wrap around */
+            rkhuint8 idx = (rkhuint8)( ( e->dynamic_ >> 6 ) - 1 );
+            rkh_exit_critical();
+
+            rkhassert( idx < RKH_DYNE_NUM_POOLS, RKH_GC_RUNOUT_POOLS );
+            rkh_dyne_put( idx, ( RKHEVT_T* )e );
+        }
+    }
+}
+
+
+HUInt 
+rkh_put_fifo( HUInt qd, RKHEVT_T *evt )
+{
+    rkh_enter_critical();
+    if( evt->dynamic_ != 0 ) 
+        ++(( RKHEVT_T* )evt)->dynamic_;
+    rkh_exit_critical();
+
+    return rkh_post_fifo( qd, evt ); 
+}
+
+
+HUInt 
+rkh_put_lifo( HUInt qd, RKHEVT_T *evt )
+{
+    rkh_enter_critical();
+    if( evt->dynamic_ != 0 ) 
+        ++(( RKHEVT_T* )evt)->dynamic_;
+    rkh_exit_critical();
+
+    return rkh_post_lifo( qd, evt ); 
+}
+
+
+#if RKH_EN_DEFERRED_EVENT == 1
+
+HUInt 
+rkh_defer( HUInt qd, RKHEVT_T *evt )
+{
+	rkh_put_fifo( qd, evt );
+}
+
+
+RKHEVT_T *
+rkh_recall( HUInt qdd, HUInt qds )
+{
+    RKHEVT_T *evt;
+	HUInt r;
+	
+										/* get an event from deferred queue */
+    if( ( r = rkh_get( qds, evt ) ) == 0 )     			/* event available? */
+	{
+		/* post it to the front of the AO's queue */
+        rkh_put_lifo( qds, evt );
+        rkh_enter_critical();
+
+        if( evt->dynamic_ != 0 )           		  /* is it a dynamic event? */
+		{
+            /* 
+			 * After posting to the AO's queue the event must be referenced
+             * at least twice: once in the deferred event queue (eq->get()
+             * did NOT decrement the reference counter) and once in the
+             * AO's event queue.
+             */
+            rkhassert( (evt->dynamic_&0x3F) > 1, RKH_RC_FAIL );
+
+            /* 
+			 * We need to decrement the reference counter once, to account
+             * for removing the event from the deferred event queue.
+             */
+            --( ( RKHEVT_T* )evt )->dynamic_;
+        }
+		rkh_exit_critical();
+    }
+    return r != 0 ? NULL : evt;
+}
+
+#endif
 
 #endif
