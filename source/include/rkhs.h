be6680bb2c8a60619c297bb8936a5d223a17efeb
vortexleo <francuccilea@gmail.com>
2012-03-27 11:48:35 +0000

    In transition to v2.0. Win32 single thread test.

--- source/include/rkhs.h
+++ source/include/rkhs.h
@@ -44,9 +44,7 @@
 	#define RKH_EQ_TYPE				RKHRQ_T
 
 	#define RKH_SMA_BLOCK( sma ) 							\
-				do {										\
-					RKHASSERT( ((RKHSMA_T*)(sma))->equeue.qty != 0 );
-				} while( 0 )
+					RKHASSERT( ((RKHSMA_T*)(sma))->equeue.qty != 0 )
 
 	#define RKH_SMA_READY( rg, sma ) 						\
 					rkh_rdy_ins( (rg), ((RKHSMA_T*)(sma))->romrkh->prio )
