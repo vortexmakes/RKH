8489bb43a87fd2a40dd1145a7725d2999356a72d
vortexleo <francuccilea@gmail.com>
2013-03-11 18:18:27 +0000

    Change blinky demo directories.

---
+++ demo/cross/blinky/platform/80x86/win32_st/vc08/bsp.h
@@ -0,0 +1,39 @@
+/*
+ *	file: bsp.h
+ *	Last updated for version: 2.0
+ *	Date of the last update:  Feb 28, 2012
+ *
+ * 	Copyright (C) 2010 Leandro Francucci. All rights reserved.
+ *
+ * 	RKH is free software: you can redistribute it and/or modify
+ * 	it under the terms of the GNU General Public License as published by
+ * 	the Free Software Foundation, either version 3 of the License, or
+ * 	(at your option) any later version.
+ *
+ *  RKH is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with RKH, see copying.txt file.
+ *
+ * Contact information:
+ * RKH web site:	http://sourceforge.net/projects/rkh-reactivesys/
+ * e-mail:			francuccilea@gmail.com
+ */
+
+
+#ifndef __BSP_H__
+#define __BSP_H__
+
+
+#include "rkh.h"
+
+
+void bsp_init( int argc, char *argv[] );
+void bsp_led_on( void );
+void bsp_led_off( void );
+
+
+#endif
