be2a56b195e91f266fc3ecae035268043c213f28
vortexleo <francuccilea@gmail.com>
2013-03-11 18:35:28 +0000

    Added arm-cortex directory in \source\portable.

---
+++ source/portable/arm-cortex/rkhs/arm_cm4f/cw_v10/rkhport.c
@@ -0,0 +1,52 @@
+/*
+ *	file: rkht.h - Freescale ColdFire V1 MCU's, CodeWarrior port
+ *	Last updated for version: 1.0.00
+ *	Date of the last update:  Mar 26, 2012
+ *
+ * 	Copyright (C) 2010 Leandro Francucci. All rights reserved.
+ *
+ * 	RKH is free software: you can redistribute it and/or modify
+ * 	it under the terms of the GNU General Public License as published by
+ * 	the Free Software Foundation, either version 3 of the License, or
+ * 	(at your option) any later version.
+ *
+ *  RKH is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with RKH, see copying.txt file.
+ *
+ * Contact information:
+ * RKH web site:	http://sourceforge.net/projects/rkh-reactivesys/
+ * e-mail:			francuccilea@gmail.com
+ */
+
+/*
+ * 	rkhport.c
+ */
+
+
+#include "rkh.h"
+
+
+RKH_MODULE_NAME( rkhport )
+RKH_MODULE_VERSION( rkhport, 1.00 )
+RKH_MODULE_DESC( rkhport, "ARM Cortex M4F" )
+
+
+const 
+char *
+rkh_get_port_version( void )
+{
+	return RKH_MODULE_GET_VERSION();
+}
+
+
+const 
+char *
+rkh_get_port_desc( void )
+{
+	return RKH_MODULE_GET_DESC();
+}
