2778b28c1cc6d1bdeb0a0acd52517a918371ffa3
vortextech <francuccilea@gmail.com>
2014-04-16 17:01:36 +0000

    Add a new include file, rkhdef.h.

---
+++ source/include/rkhdef.h
@@ -0,0 +1,67 @@
+/*
+ *	file: rkhdef.h
+ *	Last updated for version: 2.4.00
+ *	Date of the last update:  April 15, 2014
+ *
+ * 	Copyright (C) 2010 Leandro Francucci. All rights reserved.
+ *
+ * 	RKH is free software: you can redistribute it and/or modify
+ * 	it under the terms of the GNU General Public License as published by
+ * 	the Free Software Foundation, either version 3 of the License, or
+ * 	(at your option) any later version.
+ *
+ *  RKH is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with RKH, see copying.txt file.
+ *
+ * Contact information:
+ * RKH web site:	http://sourceforge.net/projects/rkh-reactivesys/
+ * e-mail:			lf@vxtsolutions.com.ar
+ */
+
+/**
+ * 	\file rkhdef.h
+ *	\brief
+ *	Assert definitions.
+ *	
+ */
+
+
+#ifndef __RKHDEF_H__
+#define __RKHDEF_H__
+
+
+#define  RKH_DEF_NULL               ((void *)0)
+
+/* ----------------- BOOLEAN DEFINES ------------------ */
+
+#define  RKH_DEF_FALSE				0u
+#define  RKH_DEF_TRUE               1u
+
+#define  RKH_DEF_NO                 0u
+#define  RKH_DEF_YES                1u
+
+#define  RKH_DEF_DISABLED           0u
+#define  RKH_DEF_ENABLED			1u
+
+#define  RKH_DEF_INACTIVE           0u
+#define  RKH_DEF_ACTIVE             1u
+
+#define  RKH_DEF_INVALID            0u
+#define  RKH_DEF_VALID              1u
+
+#define  RKH_DEF_OFF                0u
+#define  RKH_DEF_ON                 1u
+
+#define  RKH_DEF_CLR                0u
+#define  RKH_DEF_SET                1u
+
+#define  RKH_DEF_FAIL               0u
+#define  RKH_DEF_OK                 1u
+
+
+#endif
