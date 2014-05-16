5d1fdcb36ae80aac17a8c1d9430813b2e2665ec2
vortextech <francuccilea@gmail.com>
2014-05-16 02:37:15 +0000

    Working with the header of files in doxygen

--- source/include/rkhtrc.h
+++ source/include/rkhtrc.h
@@ -1,32 +1,6 @@
-/*
- *	file: rkhtrc.h
- *	Last updated for version: 1.0.00
- *	Date of the last update:  May 28, 2010
- *
- * 	Copyright (C) 2010 Leandro Francucci. All rights reserved.
- *
- * 	RKH is free software: you can redistribute it and/or modify
- * 	it under the terms of the GNU General Public License as published by
- * 	the Free Software Foundation, either version 3 of the License, or
- * 	(at your option) any later version.
- *
- *  RKH is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with RKH, see copying.txt file.
- *
- * Contact information:
- * RKH web site:	http://sourceforge.net/projects/rkh-reactivesys/
- * e-mail:			lf@vxtsolutions.com.ar
- */
-
 /**
- * 	\file rkhtrc.h
- * 	\brief
- *	Platform-independent interface for RKH trace facility.
+ * 	\file
+ * 	\brief Platform-independent interface for RKH trace facility.
  *
  * 	When a program needs to be traced, it has to generate some information 
  * 	each time it reaches a "significant step" (certain instruction in the 
@@ -67,6 +41,40 @@
  *	applied, removed or changed at any time.
  *
  *	\sa RKH_TRC_EVENTS the enumerated list of RKH trace events.
+ *
+ * 	\cond
+ *  --------------------------------------------------------------------------
+ *	Last updated for version: 2.4.04
+ *	Date of the last update:  May 15, 2014
+ *	By:						  LF
+ *  --------------------------------------------------------------------------
+ *                                Framework RKH
+ *                                -------------
+ *
+ * 	          State-machine framework for reactive embedded systems            
+ * 	        
+ * 	                    Copyright (C) 2010 Leandro Francucci.
+ * 	        All rights reserved. Protected by international copyright laws.
+ *
+ *
+ * 	RKH is free software: you can redistribute it and/or modify it under the 
+ * 	terms of the GNU General Public License as published by the Free Software 
+ * 	Foundation, either version 3 of the License, or (at your option) any 
+ * 	later version.
+ *
+ *  RKH is distributed in the hope that it will be useful, but WITHOUT ANY 
+ *  WARRANTY; without even the implied warranty of MERCHANTABILITY or 
+ *  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for 
+ *  more details.
+ *
+ *  You should have received a copy of the GNU General Public License along 
+ *  with RKH, see copying.txt file.
+ *
+ * 	Contact information:
+ * 	RKH web site:	http://sourceforge.net/projects/rkh-reactivesys/
+ * 	e-mail:			francuccilea@gmail.com
+ *  --------------------------------------------------------------------------
+ * 	\endcond
  */
 
 
