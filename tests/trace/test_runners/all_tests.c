b6611606a6fc320c5e2a91ad34316734c1cbf954
vortextech <francuccilea@gmail.com>
2015-11-19 18:05:37 +0000

    Added the directories to support TDD with Unity. Also, included the interface and its early implementation of unit test with Trazer (unitrazer.h/unitrazer.c respectively).

---
+++ tests/trace/test_runners/all_tests.c
@@ -0,0 +1,88 @@
+/*
+ *  --------------------------------------------------------------------------
+ *
+ *                                Framework RKH
+ *                                -------------
+ *
+ *            State-machine framework for reactive embedded systems
+ *
+ *                      Copyright (C) 2010 Leandro Francucci.
+ *          All rights reserved. Protected by international copyright laws.
+ *
+ *
+ *  RKH is free software: you can redistribute it and/or modify it under the
+ *  terms of the GNU General Public License as published by the Free Software
+ *  Foundation, either version 3 of the License, or (at your option) any
+ *  later version.
+ *
+ *  RKH is distributed in the hope that it will be useful, but WITHOUT ANY
+ *  WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ *  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ *  more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with RKH, see copying.txt file.
+ *
+ *  Contact information:
+ *  RKH web site:   http://sourceforge.net/projects/rkh-reactivesys/
+ *  e-mail:         francuccilea@gmail.com
+ *  ---------------------------------------------------------------------------
+ */
+
+/**
+ *  \file       all_tests.c
+ *  \ingroup    Test
+ *
+ *  \brief      ---
+ */
+
+/* -------------------------- Development history -------------------------- */
+/*
+ *  2015.11.11  LeFr  v2.4.05  ---
+ */
+
+/* -------------------------------- Authors -------------------------------- */
+/*
+ *  LeFr  Leandro Francucci  francuccilea@gmail.com
+ */
+
+/* --------------------------------- Notes --------------------------------- */
+/* ----------------------------- Include files ----------------------------- */
+
+#include "unity_fixture.h"
+#include "unitrazer.h"
+#include "rkh.h"
+
+
+/* ----------------------------- Local macros ------------------------------ */
+/* ------------------------------- Constants ------------------------------- */
+/* ---------------------------- Local data types --------------------------- */
+/* ---------------------------- Global variables --------------------------- */
+/* ---------------------------- Local variables ---------------------------- */
+/* ----------------------- Local function prototypes ----------------------- */
+/* ---------------------------- Local functions ---------------------------- */
+
+static 
+void 
+runAllTests(void)
+{
+	RUN_TEST_GROUP(trace);
+}
+
+/* ---------------------------- Global functions --------------------------- */
+
+void
+main(void)
+{
+	static int argc;
+	static const char *argv[2];
+
+	argc = 1;
+	argv[0] = "rkhtrc.c";
+	UnityMain(argc, argv, runAllTests);
+
+	for(;;)
+        ;
+}
+
+/* ------------------------------ End of file ------------------------------ */
---
+++ tests/trace/test_trace.c
@@ -0,0 +1,79 @@
+/*
+ *  --------------------------------------------------------------------------
+ *
+ *                                Framework RKH
+ *                                -------------
+ *
+ *            State-machine framework for reactive embedded systems
+ *
+ *                      Copyright (C) 2010 Leandro Francucci.
+ *          All rights reserved. Protected by international copyright laws.
+ *
+ *
+ *  RKH is free software: you can redistribute it and/or modify it under the
+ *  terms of the GNU General Public License as published by the Free Software
+ *  Foundation, either version 3 of the License, or (at your option) any
+ *  later version.
+ *
+ *  RKH is distributed in the hope that it will be useful, but WITHOUT ANY
+ *  WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ *  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ *  more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with RKH, see copying.txt file.
+ *
+ *  Contact information:
+ *  RKH web site:   http://sourceforge.net/projects/rkh-reactivesys/
+ *  e-mail:         francuccilea@gmail.com
+ *  ---------------------------------------------------------------------------
+ */
+
+/**
+ *  \file       test_sm.c
+ *  \ingroup    Test
+ *
+ *  \brief      Unit test for RKH's trace module.
+ */
+
+/* -------------------------- Development history -------------------------- */
+/*
+ *  2015.11.19  LeFr  v2.4.05  ---
+ */
+
+/* -------------------------------- Authors -------------------------------- */
+/*
+ *  LeFr  Leandro Francucci  francuccilea@gmail.com
+ */
+
+/* --------------------------------- Notes --------------------------------- */
+/* ----------------------------- Include files ----------------------------- */
+
+#include "unity_fixture.h"
+#include "rkh.h"
+
+/* ----------------------------- Local macros ------------------------------ */
+/* ------------------------------- Constants ------------------------------- */
+/* ---------------------------- Local data types --------------------------- */
+/* ---------------------------- Global variables --------------------------- */
+
+TEST_GROUP(trace);
+
+/* ---------------------------- Local variables ---------------------------- */
+/* ----------------------- Local function prototypes ----------------------- */
+/* ---------------------------- Local functions ---------------------------- */
+/* ---------------------------- Global functions --------------------------- */
+
+TEST_SETUP(trace)
+{
+}
+
+TEST_TEAR_DOWN(trace)
+{
+}
+
+TEST(trace, simpleTransition)
+{
+}
+
+/* ------------------------------ End of file ------------------------------ */
