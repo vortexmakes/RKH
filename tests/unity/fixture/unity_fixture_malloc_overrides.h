66b7bd56f44f38c2f7da032bb04d94b85353af97
vortextech <francuccilea@gmail.com>
2015-11-20 17:53:02 +0000

    Fixed a bug to allocation function redefinition in unity_fixture_malloc_overrides.h (only for MVC)

--- tests/unity/fixture/unity_fixture_malloc_overrides.h
+++ tests/unity/fixture/unity_fixture_malloc_overrides.h
@@ -32,10 +32,25 @@
    extern void UNITY_FIXTURE_FREE(void *ptr);
 #endif
 
+#if 0   /* 
+         * Only for using MSC to avoid the :
+         * "error C2375: redefinition; different linkage".
+         * For using the harness provided by unity fixture on memory 
+         * allocation function calls must be explicitely invoked the 
+         * unity_free/ma/ca/realloc() functions.For example: 
+         * #if FOOBAR
+         *     #define myMalloc     malloc
+         * #else
+         *     #define myMalloc     unity_malloc
+         * #endif
+         * ...
+         * p = myMalloc(10);
+         */
 #define malloc  unity_malloc
 #define calloc  unity_calloc
 #define realloc unity_realloc
 #define free    unity_free
+#endif
 
 void* unity_malloc(size_t size);
 void* unity_calloc(size_t num, size_t size);
