8efb84f67fe04f91ba40e3f0e7cd8e47b711ea5c
dariosb <dariosb@gmail.com>
2016-01-04 12:56:08 +0000

    update utrazer branch with 2.4.5 branch

---
+++ utrazer/tests/trace/platform/80x86/vc/rkhassert_stub.h
@@ -0,0 +1,83 @@
+/*
+ *  --------------------------------------------------------------------------
+ *
+ *                                Framework RKH
+ *                                -------------
+ *
+ *            State-machine framework for reactive embedded systems
+ *
+ *                      Copyright (C) 2010 Leandro Francucci.
+ *          All rights reserved. Protected by international copyright laws.
+ *
+ *
+ *  RKH is free software: you can redistribute it and/or modify it under the
+ *  terms of the GNU General Public License as published by the Free Software
+ *  Foundation, either version 3 of the License, or (at your option) any
+ *  later version.
+ *
+ *  RKH is distributed in the hope that it will be useful, but WITHOUT ANY
+ *  WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ *  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ *  more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with RKH, see copying.txt file.
+ *
+ *  Contact information:
+ *  RKH web site:   http://sourceforge.net/projects/rkh-reactivesys/
+ *  e-mail:         francuccilea@gmail.com
+ *  ---------------------------------------------------------------------------
+ */
+
+/**
+ *  \file       rkhassert_stub.c
+ *  \ingroup    Test
+ *
+ *  \brief      Assertion stub for 80x86 OS win32
+ */
+
+/* -------------------------- Development history -------------------------- */
+/*
+ *  2015.10.24  LeFr  v2.4.05  ---
+ */
+
+/* -------------------------------- Authors -------------------------------- */
+/*
+ *  LeFr  Leandro Francucci  francuccilea@gmail.com
+ */
+
+/* --------------------------------- Notes --------------------------------- */
+/* --------------------------------- Module -------------------------------- */
+
+#ifndef __RKHASSERT_STUB_H__
+#define __RKHASSERT_STUB_H__
+
+/* ----------------------------- Include files ----------------------------- */
+
+#include "rkh.h"
+
+/* ---------------------- External C language linkage ---------------------- */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* --------------------------------- Macros -------------------------------- */
+/* -------------------------------- Constants ------------------------------ */
+/* ------------------------------- Data types ------------------------------ */
+/* -------------------------- External variables --------------------------- */
+/* -------------------------- Function prototypes -------------------------- */
+
+void rkh_assertStub_reset(void);
+const char *rkh_assertStub_getLastError(void);
+
+/* -------------------- External C language linkage end -------------------- */
+
+#ifdef __cplusplus
+}
+#endif
+
+/* ------------------------------ Module end ------------------------------- */
+
+#endif
+/* ------------------------------ End of file ------------------------------ */
