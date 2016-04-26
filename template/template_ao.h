a99f85e9bb8441a16b3feaed7179d82b9dea041f
vortextech <francuccilea@gmail.com>
2016-04-26 18:57:51 +0000

    Added template files for dealing with implementation and specification of active objects.

---
+++ template/template_ao.h
@@ -0,0 +1,54 @@
+/**
+ *  \file       template_ao.h
+ *  \ingroup    template
+ *
+ *  \brief      Template for module specification of an active object.
+ *              Active object specification.
+ *
+ *  This file contains the definitions of objet structures (state machine, 
+ *  states, and pseudostates) and other facilities shared among the 
+ *  components of the application.
+ */
+
+/* -------------------------- Development history -------------------------- */
+/*
+ *  2016.03.17  LeFr  v1.0.00  Initial version
+ */
+
+/* -------------------------------- Authors -------------------------------- */
+/*
+ *  LeFr  Leandro Francucci  francuccilea@gmail.com
+ */
+
+/* --------------------------------- Notes --------------------------------- */
+/* --------------------------------- Module -------------------------------- */
+#ifndef __TEMPLATE_AO_H__
+#define __TEMPLATE_AO_H__
+
+/* ----------------------------- Include files ----------------------------- */
+#include "rkh.h"
+
+/* ---------------------- External C language linkage ---------------------- */
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* --------------------------------- Macros -------------------------------- */
+/* -------------------------------- Constants ------------------------------ */
+
+/* ================================ Signals ================================ */
+/* ======================== Declares active object ========================= */
+/* =================== Declares states and pseudostates ==================== */
+
+/* ------------------------------- Data types ------------------------------ */
+/* -------------------------- External variables --------------------------- */
+/* -------------------------- Function prototypes -------------------------- */
+/* -------------------- External C language linkage end -------------------- */
+#ifdef __cplusplus
+}
+#endif
+
+/* ------------------------------ Module end ------------------------------- */
+#endif
+
+/* ------------------------------ End of file ------------------------------ */
