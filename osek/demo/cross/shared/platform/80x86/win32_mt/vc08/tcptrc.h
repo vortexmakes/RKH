adce8dd95c71af5faabff7991027fa3aa7ea7937
dariosb <dariosb@gmail.com>
2016-07-07 17:57:09 +0000

    shared 80x86 bsp uncrustified

--- osek/demo/cross/shared/platform/80x86/win32_mt/vc08/tcptrc.h
+++ osek/demo/cross/shared/platform/80x86/win32_mt/vc08/tcptrc.h
@@ -1,100 +1,52 @@
 /**
- * \cond
- *  --------------------------------------------------------------------------
+ *  \file       tcptrc.h
+ *  \ingroup    bsp
  *
- *                                Framework RKH
- *                                -------------
- *
- * 	          State-machine framework for reactive embedded systems            
- * 	        
- * 	                    Copyright (C) 2010 Leandro Francucci.
- * 	        All rights reserved. Protected by international copyright laws.
- *
- *
- * 	RKH is free software: you can redistribute it and/or modify it under the 
- * 	terms of the GNU General Public License as published by the Free Software 
- * 	Foundation, either version 3 of the License, or (at your option) any 
- * 	later version.
- *
- *  RKH is distributed in the hope that it will be useful, but WITHOUT ANY 
- *  WARRANTY; without even the implied warranty of MERCHANTABILITY or 
- *  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for 
- *  more details.
- *
- *  You should have received a copy of the GNU General Public License along 
- *  with RKH, see copying.txt file.
- *
- * 	Contact information:
- * 	RKH web site:	http://sourceforge.net/projects/rkh-reactivesys/
- * 	e-mail:			francuccilea@gmail.com
- *
- *  --------------------------------------------------------------------------
- *  File                     : tcptrc.h
- *	Last updated for version : v2.4.04
- *	By                       : DB
- *	                           LF
- *  --------------------------------------------------------------------------
- *  \endcond
- *
- * 	\file
- * 	\ingroup 	prt
- *
- * 	\brief 		Socket TCP/IP support for 80x86 OS win32
- *
- * 	\note	    (1) To use the windows socket must be added the "ws2_32.lib" 
- * 					library in Linker->Input->Additional Dependencies 
- * 					configuration.
+ *  \brief      Socket TCP/IP support for 80x86 OS Win32
+ */
+/* -------------------------- Development history -------------------------- */
+/*
+ *  2015.10.24  DaBa  v1.0.00  Initial version
  */
 
+/* -------------------------------- Authors -------------------------------- */
+/*
+ *  DaBa  Darío Baliña  dariosb@gmail.com
+ */
+
+/* --------------------------------- Notes --------------------------------- */
+/*
+ *  (1) To use the windows socket must be added the "ws2_32.lib"
+ *      library in Linker->Input->Additional Dependencies
+ *      configuration.
+ */
 
 #ifndef __TCPTRC_H__
 #define __TCPTRC_H__
 
-
+/* ----------------------------- Include files ----------------------------- */
 #include <winsock.h>
-	
 
-/**
- * 	\brief
- *	---.
- * 
- *	---.
- *
- * 	\param port 		---.
- * 	\param srvip 		---.
- * 	\param ps 			---.
- *
- * 	\returns
- * 	---.
- */
-
-int tcp_trace_open( unsigned short port, char *srvip, SOCKET *ps );
-
-
-/**
- * 	\brief
- *	---.
- * 
- *	---.
- *
- * 	\param s 			---.
- * 	\param c 			---.
- * 	\param len 			---.
- */
-
-void tcp_trace_send( SOCKET s, const char *buf, int len );
-
-
-/**
- * 	\brief
- *	---.
- * 
- *	---.
- *
- * 	\param s	 		---.
- */
-
-void tcp_trace_close( SOCKET s );
-
-
+/* ---------------------- External C language linkage ---------------------- */
+#ifdef __cplusplus
+extern "C" {
 #endif
+
+/* --------------------------------- Macros -------------------------------- */
+/* -------------------------------- Constants ------------------------------ */
+/* ------------------------------- Data types ------------------------------ */
+/* -------------------------- External variables --------------------------- */
+/* -------------------------- Function prototypes -------------------------- */
+int tcp_trace_open(unsigned short port, char *srvip, SOCKET *ps);
+void tcp_trace_send(SOCKET s, const char *buf, int len);
+void tcp_trace_close(SOCKET s);
+
+/* -------------------- External C language linkage end -------------------- */
+#ifdef __cplusplus
+}
+#endif
+
+/* ------------------------------ Module end ------------------------------- */
+#endif
+
+/* ------------------------------ End of file ------------------------------ */
--- osek/demo/cross/shared/platform/80x86/win32_st/vc08/tcptrc.c
+++ osek/demo/cross/shared/platform/80x86/win32_st/vc08/tcptrc.c
@@ -1,116 +1,103 @@
 /**
- * \cond
- *  --------------------------------------------------------------------------
+ *  \file       tcptrc.c
+ *  \ingroup    bsp
  *
- *                                Framework RKH
- *                                -------------
- *
- * 	          State-machine framework for reactive embedded systems            
- * 	        
- * 	                    Copyright (C) 2010 Leandro Francucci.
- * 	        All rights reserved. Protected by international copyright laws.
- *
- *
- * 	RKH is free software: you can redistribute it and/or modify it under the 
- * 	terms of the GNU General Public License as published by the Free Software 
- * 	Foundation, either version 3 of the License, or (at your option) any 
- * 	later version.
- *
- *  RKH is distributed in the hope that it will be useful, but WITHOUT ANY 
- *  WARRANTY; without even the implied warranty of MERCHANTABILITY or 
- *  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for 
- *  more details.
- *
- *  You should have received a copy of the GNU General Public License along 
- *  with RKH, see copying.txt file.
- *
- * 	Contact information:
- * 	RKH web site:	http://sourceforge.net/projects/rkh-reactivesys/
- * 	e-mail:			francuccilea@gmail.com
- *
- *  --------------------------------------------------------------------------
- *  File                     : tcptrc.c
- *	Last updated for version : v2.4.04
- *	By                       : DB
- *  --------------------------------------------------------------------------
- *  \endcond
- *
- * 	\file
- * 	\ingroup 	prt
- *
- * 	\brief 		Socket TCP/IP support for 80x86 OS win32
+ *  \brief      Socket TCP/IP support for 80x86 OS Win32
  */
 
+/* -------------------------- Development history -------------------------- */
+/*
+ *  2015.10.24  DaBa  v1.0.00  Initial version
+ */
 
+/* -------------------------------- Authors -------------------------------- */
+/*
+ *  DaBa  Darío Baliña  dariosb@gmail.com
+ */
+
+/* --------------------------------- Notes --------------------------------- */
+/*
+ *  (1) To use the windows socket must be added the "ws2_32.lib"
+ *      library in Linker->Input->Additional Dependencies
+ *      configuration.
+ */
+
+/* ----------------------------- Include files ----------------------------- */
 #include "tcptrc.h"
 #include "rkh.h"
 #include <stdio.h>
 
-
+/* ----------------------------- Local macros ------------------------------ */
+/* ------------------------------- Constants ------------------------------- */
+/* ---------------------------- Local data types --------------------------- */
+/* ---------------------------- Global variables --------------------------- */
+/* ---------------------------- Local variables ---------------------------- */
+/* ----------------------- Local function prototypes ----------------------- */
+/* ---------------------------- Local functions ---------------------------- */
+/* ---------------------------- Global functions --------------------------- */
 #if RKH_CFG_TRC_EN == 1
 
 int
-tcp_trace_open( unsigned short port, char *srv_ip, SOCKET *ps )
+tcp_trace_open(unsigned short port, char *srv_ip, SOCKET *ps)
 {
-	WORD wVersionRequested;
-	WSADATA wsaData;
-	SOCKADDR_IN target; /* Socket address information */
-	SOCKET s;
-	int err;
+    WORD wVersionRequested;
+    WSADATA wsaData;
+    SOCKADDR_IN target; /* Socket address information */
+    SOCKET s;
+    int err;
 
-	/* --- INITIALIZATION ----------------------------------- */
-	wVersionRequested = MAKEWORD( 1, 1 );
-	err = WSAStartup( wVersionRequested, &wsaData );
-	*ps = INVALID_SOCKET;
+    /* --- INITIALIZATION ----------------------------------- */
+    wVersionRequested = MAKEWORD(1, 1);
+    err = WSAStartup(wVersionRequested, &wsaData);
+    *ps = INVALID_SOCKET;
 
-	if( err != 0 )
-	{
-		printf("WSAStartup error %ld", WSAGetLastError() );
-		WSACleanup();
-		return -1;
-	}
-	/* ------------------------------------------------------ */
-	
-	/* ---- build address structure to bind to socket.-------- */
-	target.sin_family = AF_INET; /* address family Internet */
-	target.sin_port = htons( port ); /* Port to connect on */
-	target.sin_addr.s_addr = inet_addr (srv_ip); /* Server IP */
-	/* ------------------------------------------------------ */
-	
-	/* ---- create SOCKET-------------------------------------- */
-	s = socket (AF_INET, SOCK_STREAM, IPPROTO_TCP); /* Create socket */
-	if( s == INVALID_SOCKET )
-	{
-		printf("socket error %ld" , WSAGetLastError() );
-		WSACleanup();
-		return -1; /* Couldn't create the socket */
-	}  
-	/* ------------------------------------------------------ */
+    if (err != 0)
+    {
+        printf("WSAStartup error %ld", WSAGetLastError());
+        WSACleanup();
+        return -1;
+    }
+    /* ------------------------------------------------------ */
 
-	/* ---- try CONNECT ----------------------------------------- */
-	if( connect( s, (SOCKADDR*)&target, sizeof(target) ) == SOCKET_ERROR )
-	{
-		printf("connect error %ld", WSAGetLastError() );
-		WSACleanup();
-		return -1; /* Couldn't connect */
-	}
-	*ps = s;
-	return 0;
+    /* ---- build address structure to bind to socket.-------- */
+    target.sin_family = AF_INET; /* address family Internet */
+    target.sin_port = htons(port);   /* Port to connect on */
+    target.sin_addr.s_addr = inet_addr (srv_ip); /* Server IP */
+    /* ------------------------------------------------------ */
+
+    /* ---- create SOCKET-------------------------------------- */
+    s = socket (AF_INET, SOCK_STREAM, IPPROTO_TCP); /* Create socket */
+    if (s == INVALID_SOCKET)
+    {
+        printf("socket error %ld", WSAGetLastError());
+        WSACleanup();
+        return -1; /* Couldn't create the socket */
+    }
+    /* ------------------------------------------------------ */
+
+    /* ---- try CONNECT ----------------------------------------- */
+    if (connect(s, (SOCKADDR*)&target, sizeof(target)) == SOCKET_ERROR)
+    {
+        printf("connect error %ld", WSAGetLastError());
+        WSACleanup();
+        return -1; /* Couldn't connect */
+    }
+    *ps = s;
+    return 0;
 }
 
-
 void
-tcp_trace_send( SOCKET s, const char *buf, int len )
+tcp_trace_send(SOCKET s, const char *buf, int len)
 {
-	send( s, buf, len, 0 ); 
+    send(s, buf, len, 0);
 }
 
-
 void
-tcp_trace_close( SOCKET s )
+tcp_trace_close(SOCKET s)
 {
-	closesocket( s );
-	WSACleanup();
+    closesocket(s);
+    WSACleanup();
 }
 
 #endif
+/* ------------------------------ End of file ------------------------------ */
