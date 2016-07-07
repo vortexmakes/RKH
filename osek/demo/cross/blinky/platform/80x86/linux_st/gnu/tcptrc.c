163c77d2fce8c8de65796db3bbba159118d1c967
dariosb <dariosb@gmail.com>
2016-07-07 17:56:36 +0000

    Blinky 80x86 bsp uncrustified

--- osek/demo/cross/blinky/platform/80x86/linux_st/gnu/tcptrc.c
+++ osek/demo/cross/blinky/platform/80x86/linux_st/gnu/tcptrc.c
@@ -1,47 +1,22 @@
 /**
- * \cond
- *  --------------------------------------------------------------------------
+ *  \file       tcptrc.c
+ *  \ingroup    bsp
  *
- *                                Framework RKH
- *                                -------------
- *
- * 	          State-machine framework for reactive embedded systems            
- * 	        
- * 	                    Copyright (C) 2010 Leandro Francucci.
- * 	        All rights reserved. Protected by international copyright laws.
- *
- *
- * 	RKH is free software: you can redistribute it and/or modify it under the 
- * 	terms of the GNU General Public License as published by the Free Software 
- * 	Foundation, either version 3 of the License, or (at your option) any 
- * 	later version.
- *
- *  RKH is distributed in the hope that it will be useful, but WITHOUT ANY 
- *  WARRANTY; without even the implied warranty of MERCHANTABILITY or 
- *  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for 
- *  more details.
- *
- *  You should have received a copy of the GNU General Public License along 
- *  with RKH, see copying.txt file.
- *
- * 	Contact information:
- * 	RKH web site:	http://sourceforge.net/projects/rkh-reactivesys/
- * 	e-mail:			francuccilea@gmail.com
- *
- *  --------------------------------------------------------------------------
- *  File                     : tcptrc.c
- *	Last updated for version : v2.4.04
- *	By                       : JC
- *  --------------------------------------------------------------------------
- *  \endcond
- *
- * 	\file
- * 	\ingroup 	prt
- *
- * 	\brief 		Socket TCP/IP support for 80x86 OS linux
+ *  \brief      Socket TCP/IP support for 80x86 OS linux
  */
 
+/* -------------------------- Development history -------------------------- */
+/*
+ *  2015.10.24  DaBa  v1.0.00  Initial version
+ */
 
+/* -------------------------------- Authors -------------------------------- */
+/*
+ *  DaBa  Darío Baliña  dariosb@gmail.com
+ */
+
+/* --------------------------------- Notes --------------------------------- */
+/* ----------------------------- Include files ----------------------------- */
 #include "tcptrc.h"
 #include "rkh.h"
 #include <stdio.h>
@@ -54,61 +29,67 @@
 #include <arpa/inet.h>
 #include <netdb.h>
 
-
+/* ----------------------------- Local macros ------------------------------ */
+/* ------------------------------- Constants ------------------------------- */
+/* ---------------------------- Local data types --------------------------- */
+/* ---------------------------- Global variables --------------------------- */
+/* ---------------------------- Local variables ---------------------------- */
+/* ----------------------- Local function prototypes ----------------------- */
+/* ---------------------------- Local functions ---------------------------- */
+/* ---------------------------- Global functions --------------------------- */
 #if RKH_CFG_TRC_EN == 1
 
 int
-tcp_trace_open( unsigned short port, char *srv_ip, int *ps )
+tcp_trace_open(unsigned short port, char *srv_ip, int *ps)
 {
-	int s;
-	struct sockaddr_in serv_addr;
+    int s;
+    struct sockaddr_in serv_addr;
 
+    /* --- INITIALIZATION ----------------------------------- */
+    *ps = 0;
 
-	/* --- INITIALIZATION ----------------------------------- */
-	*ps = 0;
+    /* ------------------------------------------------------ */
 
-	/* ------------------------------------------------------ */
-	
-	/* ---- create SOCKET-------------------------------------- */
-	s = socket (AF_INET, SOCK_STREAM, IPPROTO_TCP); /* Create socket */
-	if( s < 0 )
-	{
-		printf("socket opening error\n" );
-		return -1; /* Couldn't create the socket */
-	}  
+    /* ---- create SOCKET-------------------------------------- */
+    s = socket (AF_INET, SOCK_STREAM, IPPROTO_TCP); /* Create socket */
+    if (s < 0)
+    {
+        printf("socket opening error\n");
+        return -1; /* Couldn't create the socket */
+    }
 
-
-	bzero((char *) &serv_addr, sizeof(serv_addr));
+    bzero((char *) &serv_addr, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
 
-	serv_addr.sin_addr.s_addr = inet_addr(srv_ip);  /* Normal Address */
+    serv_addr.sin_addr.s_addr = inet_addr(srv_ip);  /* Normal Address */
 
     serv_addr.sin_port = htons(port);
-	/* ------------------------------------------------------ */
+    /* ------------------------------------------------------ */
 
-	/* ---- try CONNECT ----------------------------------------- */
-	if (connect(s,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
-	{
-		printf("ERROR connecting \n");
-		return -1;
-	}
-		
-	*ps = s;
-	return 0;
+    /* ---- try CONNECT ----------------------------------------- */
+    if (connect(s,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0)
+    {
+        printf("ERROR connecting \n");
+        return -1;
+    }
+
+    *ps = s;
+    return 0;
 }
 
-
 void
-tcp_trace_send( int s, const char *buf, int len )
+tcp_trace_send(int s, const char *buf, int len)
 {
-    write( s, buf, len );
+    write(s, buf, len);
 }
 
-
 void
-tcp_trace_close( int s )
+tcp_trace_close(int s)
 {
-	close( s );
+    close(s);
 }
 
 #endif
+
+/* ------------------------------ End of file ------------------------------ */
+
--- osek/demo/cross/blinky/platform/80x86/linux_st/gnu/tcptrc.h
+++ osek/demo/cross/blinky/platform/80x86/linux_st/gnu/tcptrc.h
@@ -1,92 +1,47 @@
 /**
- * \cond
- *  --------------------------------------------------------------------------
+ *  \file       tcptrc.h
+ *  \ingroup    bsp
  *
- *                                Framework RKH
- *                                -------------
- *
- * 	          State-machine framework for reactive embedded systems            
- * 	        
- * 	                    Copyright (C) 2010 Leandro Francucci.
- * 	        All rights reserved. Protected by international copyright laws.
- *
- *
- * 	RKH is free software: you can redistribute it and/or modify it under the 
- * 	terms of the GNU General Public License as published by the Free Software 
- * 	Foundation, either version 3 of the License, or (at your option) any 
- * 	later version.
- *
- *  RKH is distributed in the hope that it will be useful, but WITHOUT ANY 
- *  WARRANTY; without even the implied warranty of MERCHANTABILITY or 
- *  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for 
- *  more details.
- *
- *  You should have received a copy of the GNU General Public License along 
- *  with RKH, see copying.txt file.
- *
- * 	Contact information:
- * 	RKH web site:	http://sourceforge.net/projects/rkh-reactivesys/
- * 	e-mail:			francuccilea@gmail.com
- *
- *  --------------------------------------------------------------------------
- *  File                     : tcptrc.h
- *	Last updated for version : v2.4.04
- *	By                       : DB
- *	                           LF
- *  --------------------------------------------------------------------------
- *  \endcond
- *
- * 	\file
- * 	\ingroup 	prt
- *
- * 	\brief 		Socket TCP/IP support for 80x86 OS linux
+ *  \brief      Socket TCP/IP support for 80x86 OS linux
  */
 
+/* -------------------------- Development history -------------------------- */
+/*
+ *  2015.10.24  DaBa  v1.0.00  Initial version
+ */
 
+/* -------------------------------- Authors -------------------------------- */
+/*
+ *  LeFr  Leandro Francucci  francuccilea@gmail.com
+ *  DaBa  Darío Baliña  dariosb@gmail.com
+ */
+
+/* --------------------------------- Notes --------------------------------- */
 #ifndef __TCPTRC_H__
 #define __TCPTRC_H__
 
-
-/**
- * 	\brief
- *	---.
- * 
- *	---.
- *
- * 	\param port 		---.
- * 	\param srvip 		---.
- * 	\param ps 			---.
- *
- * 	\returns
- * 	---.
- */
-
-int tcp_trace_open( unsigned short port, char *srvip, int *ps );
-
-
-/**
- * 	\brief
- *	---.
- * 
- *	---.
- *
- * 	\param s 			---.
- * 	\param c 			---.
- */
-
-void tcp_trace_send( int s, const char *buf, int len );
-
-
-/**
- * 	\brief
- *	---.
- * 
- *	---.
- *
- * 	\param s	 		---.
- */
-
-void tcp_trace_close( int s );
-
-
+/* ----------------------------- Include files ----------------------------- */
+/* ---------------------- External C language linkage ---------------------- */
+#ifdef __cplusplus
+extern "C" {
 #endif
+
+/* --------------------------------- Macros -------------------------------- */
+/* -------------------------------- Constants ------------------------------ */
+/* ------------------------------- Data types ------------------------------ */
+/* -------------------------- External variables --------------------------- */
+/* -------------------------- Function prototypes -------------------------- */
+int tcp_trace_open(unsigned short port, char *srvip, int *ps);
+void tcp_trace_send(int s, const char *buf, int len);
+void tcp_trace_close(int s);
+
+/* -------------------- External C language linkage end -------------------- */
+#ifdef __cplusplus
+}
+#endif
+
+/* ------------------------------ Module end ------------------------------- */
+#endif
+
+/* ------------------------------ End of file ------------------------------ */
+
