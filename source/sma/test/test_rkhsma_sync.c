3e63aa9627711f6dccc5486e76653a8aa8efe6c0
lf <francuccilea@gmail.com>
2018-08-28 14:22:53 -0300

    Test cases of rkhsma_sync are done

--- source/sma/test/test_rkhsma_sync.c
+++ source/sma/test/test_rkhsma_sync.c
@@ -32,14 +32,20 @@
  */
 
 /**
- *  \file       test_rkhsma_sync_runner.c
+ *  \file       test_rkhsma_sync.c
  *  \ingroup    test_sma
- *  \brief      Test runner of synchronous active object module
+ *  \brief      Unit test for synchronous mechanism of sma module.
+ *
+ *  \addtogroup test
+ *  @{
+ *  \addtogroup test_sma SMA
+ *  @{
+ *  \brief      Unit test for sma module.
  */
 
 /* -------------------------- Development history -------------------------- */
 /*
- *  2017.15.05  LeFr  v2.4.05  ---
+ *  2017.05.15  LeFr  v2.4.05  ---
  */
 
 /* -------------------------------- Authors -------------------------------- */
@@ -49,22 +55,86 @@
 
 /* --------------------------------- Notes --------------------------------- */
 /* ----------------------------- Include files ----------------------------- */
-#include "unity_fixture.h"
+#include "unity.h"
+#include "rkhsma_sync.h"
+#include "Mock_rkhqueue.h"
+#include "Mock_rkhassert.h"
+#include "Mock_rkhsma_prio.h"
 
 /* ----------------------------- Local macros ------------------------------ */
 /* ------------------------------- Constants ------------------------------- */
 /* ---------------------------- Local data types --------------------------- */
 /* ---------------------------- Global variables --------------------------- */
 /* ---------------------------- Local variables ---------------------------- */
+static RKHROM RKH_ROM_T base = {0, 0, "ao"};
+static RKH_SMA_T ao;
+
 /* ----------------------- Local function prototypes ----------------------- */
 /* ---------------------------- Local functions ---------------------------- */
-/* ---------------------------- Global functions --------------------------- */
-TEST_GROUP_RUNNER(sync)
+static void 
+MockAssertCallback(const char* const file, int line, int cmock_num_calls)
 {
-	RUN_TEST_CASE(sync, BlockActiveObject);
-	RUN_TEST_CASE(sync, Fails_TriesBlockActiveObjectWithEmptyQueue);
-	RUN_TEST_CASE(sync, SetActiveObjectReady);
-	RUN_TEST_CASE(sync, SetActiveObjectUnready);
+    TEST_PASS();
 }
 
+/* ---------------------------- Global functions --------------------------- */
+void
+setUp(void)
+{
+    Mock_rkhassert_Init();
+    Mock_rkhsma_prio_Init();
+    ao.sm.romrkh = &base;
+}
+
+void
+tearDown(void)
+{
+    Mock_rkhassert_Verify();
+    Mock_rkhsma_prio_Verify();
+    Mock_rkhassert_Destroy();
+    Mock_rkhsma_prio_Destroy();
+}
+
+/**
+ *  \addtogroup test_sync Test cases of synchronous mechanism group
+ *  @{
+ *  \name Test cases of synchronous mechanism group
+ *  @{ 
+ */
+void
+test_BlockActiveObject(void)
+{
+    ao.equeue.qty = 1;
+    rkh_sma_block(&ao);
+}
+
+void
+test_Fails_TriesBlockActiveObjectWithEmptyQueue(void)
+{
+    rkh_assert_Expect("rkhsma_sync", 0);
+    rkh_assert_IgnoreArg_line();
+    rkh_assert_StubWithCallback(MockAssertCallback);
+    ao.equeue.qty = 0;
+
+    rkh_sma_block(&ao);
+}
+
+void
+test_SetActiveObjectReady(void)
+{
+    rkh_smaPrio_setReady_Expect(ao.sm.romrkh->prio);
+    rkh_sma_setReady(&ao);
+}
+
+void
+test_SetActiveObjectUnready(void)
+{
+    rkh_smaPrio_setUnready_Expect(ao.sm.romrkh->prio);
+    rkh_sma_setUnready(&ao);
+}
+
+/** @} doxygen end group definition */
+/** @} doxygen end group definition */
+/** @} doxygen end group definition */
+/** @} doxygen end group definition */
 /* ------------------------------ End of file ------------------------------ */
