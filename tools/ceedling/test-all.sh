7827113d875add80c36d69249254af513749021c
lf <francuccilea@gmail.com>
2018-09-06 15:45:05 -0300

    Working on travis file

--- tools/ceedling/test-all.sh
+++ tools/ceedling/test-all.sh
@@ -30,9 +30,9 @@ do
          echo "[ERROR] Ceedling project not found"
      else
          if [ $clobber == 0 ]; then
-             sudo ceedling clean gcov:all
+             ceedling clean gcov:all
          else
-             sudo ceedling clean clobber gcov:all
+             ceedling clean clobber gcov:all
          fi
      fi
 done
@@ -41,13 +41,13 @@ echo ""
 echo "Generating code coverage report"
 echo "-------------------------------"
 cd ..
-sudo lcov -c -d . -o ../$ceedling_dir/gcov/rkh-coverage-total.info
+lcov -c -d . -o ../$ceedling_dir/gcov/rkh-coverage-total.info
 add=()
 for module in $modules;
 do
     cd $currdir
     cd $source_dir/$module
-    sudo lcov -e ../../$ceedling_dir/gcov/rkh-coverage-total.info "$(pwd)/src/rkh$module*.c" -o ../../$ceedling_dir/gcov/$module.info
+    lcov -e ../../$ceedling_dir/gcov/rkh-coverage-total.info "$(pwd)/src/rkh$module*.c" -o ../../$ceedling_dir/gcov/$module.info
     add+=(-a $module".info")
 #   cd $source_dir/$module/test/
 #   submodules=(`find . -name "test_rkh*.c" -type f | sort -r | head -8 | sed 's/\.\\/test_//' | sed 's/\.c//'`)
@@ -56,8 +56,8 @@ do
 done
 
 cd ../../$ceedling_dir/gcov/
-sudo lcov "${add[@]}" -o rkh-coverage.info
-sudo genhtml rkh-coverage.info -o .
+lcov "${add[@]}" -o rkh-coverage.info
+genhtml rkh-coverage.info -o .
 
 exit 0
 
