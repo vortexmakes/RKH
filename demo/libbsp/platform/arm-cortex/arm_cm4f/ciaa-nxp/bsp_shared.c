27897706adb692963a85cacb1a1cd711f49a5c1a
Carlos Mancon <manconci@gmail.com>
2020-06-11 09:09:01 -0300

    Added bsp related files to demo project

--- demo/libbsp/platform/arm-cortex/arm_cm4f/ciaa-nxp/bsp_shared.c
+++ demo/libbsp/platform/arm-cortex/arm_cm4f/ciaa-nxp/bsp_shared.c
@@ -8,7 +8,7 @@
 
 /* -------------------------------- Authors -------------------------------- */
 /*
- *  CaMa  Carlos Mancòn  manconci@gmail.com
+ *  CaMa  Carlos Mancón  manconci@gmail.com
  */
 
 /* --------------------------------- Notes --------------------------------- */
@@ -47,26 +47,206 @@
 /*     in others if they are declared in those files using extern).          */
 
 /* ---------------------------- Local variables ---------------------------- */
-/* (1) Static external definitions used only in this file.                   */ 
+/* (1) Static external definitions used only in this file.                   */
+static RKH_TS_T tstamp;
+static rui32_t l_rnd;				/* random seed */
 
 /* ----------------------- Local function prototypes ----------------------- */
 /* ---------------------------- Local functions ---------------------------- */
+
+static
+void
+print_banner( void )
+{
+//TODO
+#ifdef PRINTF
+	PRINTF(	"\"Shared\" example\n\n" );
+	PRINTF(	"RKH version      = %s\n", RKH_RELEASE );
+	PRINTF(	"Port version     = %s\n", rkhport_get_version() );
+	PRINTF(	"Port description = %s\n\n", rkhport_get_desc() );
+	PRINTF(	"Description: \n\n" );
+	PRINTF(	"This application deals with the shared resource problem \n" );
+	PRINTF(	"in active object systems. Showing one of the biggest \n" );
+	PRINTF(	"benefit of using active objects: resource encapsulation. \n" );
+	PRINTF(	"The encapsulation naturally designates the owner of the \n" );
+	PRINTF(	"resource as the ultimate arbiter in resolving any contention \n" );
+	PRINTF(	"and potential conflicts for the resource. \n" );
+	PRINTF(	"The SHD application is relatively simple and can be tested \n" );
+	PRINTF(	"only with a couple of LEDs on your target board. \n" );
+	PRINTF(	"Still, SHD contains five (5) concurrent active objects \n" );
+	PRINTF(	"that exchange events via direct event posting mechanism. \n" );
+	PRINTF(	"The application uses four timers, as well as dynamic  \n" );
+	PRINTF(	"and static events. \n" );
+	PRINTF(	"On the other hand, this application could be used in either \n" );
+	PRINTF(	"preemptive or cooperative enviroment. \n" );
+	PRINTF(	"Aditionally, the SHD could be used to verify a new RKH port. \n" );
+	PRINTF(	"\n\n\n" );
+
+	PRINTF( "1.- Each Client have your own color, Client 1-4:\n" );
+	PRINTF( "	   	RED, GREEN, BLUE, YELLOW\n" );
+	PRINTF( "2.- Press SW2 to PAUSE.\n" );
+	PRINTF( "3.- Paused state is shown with a WHITE in RGB led\n." );
+#endif
+}
+
 /* ---------------------------- Global functions --------------------------- */
-void bsp_init(int argc, char *argv[]);
-void bsp_keyParser(int c);
-void bsp_timeTick(void);
-rui32_t bsp_rand( void );
-void bsp_srand( rui32_t seed );
-void bsp_cli_req( rui8_t clino );
-void bsp_cli_wait_req( rui8_t clino, RKH_TNT_T req_time );
-void bsp_cli_using( rui8_t clino, RKH_TNT_T using_time );
-void bsp_cli_paused( rui8_t clino );
-void bsp_cli_resumed( rui8_t clino );
-void bsp_cli_done( rui8_t clino );
-void bsp_svr_recall( rui8_t clino );
-void bsp_svr_paused(rui32_t ntot, rui32_t *ncr);
-void bsp_svr_start( rui8_t clino );
-void bsp_svr_end( void );
-void bsp_svr_resume( void );
+
+void
+bsp_init(int argc, char *argv[])
+{
+	rInt cn;
+
+	(void)argc;
+	(void)argv;
+
+	SystemCoreClockUpdate();
+
+	Board_Init();
+
+	bsp_srand( 1234U );
+	print_banner();
+	rkh_fwk_init();
+
+	RKH_FILTER_OFF_SMA( server );
+	for( cn = 0; cn < NUM_CLIENTS; ++cn )
+		RKH_FILTER_OFF_SMA( CLI(cn) );
+
+	RKH_FILTER_OFF_EVENT( RKH_TE_SMA_FIFO );
+	RKH_FILTER_OFF_EVENT( RKH_TE_SMA_LIFO );
+	RKH_FILTER_OFF_EVENT( RKH_TE_SM_DCH );
+	RKH_FILTER_OFF_EVENT( RKH_TE_SM_STATE );
+	/*RKH_FILTER_OFF_ALL_SIGNALS();*/
+	RKH_FILTER_OFF_SIGNAL( REQ );
+	RKH_FILTER_OFF_SIGNAL( START );
+
+	RKH_TRC_OPEN();
+}
+
+void
+bsp_keyParser(int c)
+{
+
+}
+
+void
+bsp_timeTick(void)
+{
+    ++tstamp;
+}
+
+rui32_t
+bsp_rand( void )
+{
+	/*
+	 * A very cheap pseudo-random-number generator.
+	 * "Super-Duper" Linear Congruential Generator (LCG)
+	 * LCG(2^32, 3*7*11*13*23, 0, seed) [MS]
+	 */
+	l_rnd = l_rnd * (3*7*11*13*23);
+	return l_rnd >> 8;
+}
+
+void
+bsp_srand( rui32_t seed )
+{
+    l_rnd = seed;
+}
+
+void
+bsp_cli_req( rui8_t clino )
+{
+#ifdef PRINTF
+	PRINTF( "Client[%d] - Send request to server...\n", CLI_ID(clino) );
+#endif
+}
+
+void
+bsp_cli_wait_req( rui8_t clino, RKH_TNT_T req_time )
+{
+#ifdef PRINTF
+	PRINTF( "Client[%d] - Waiting for send request to server (%d seg)\n",
+									CLI_ID(clino), req_time );
+#endif
+}
+
+void
+bsp_cli_using( rui8_t clino, RKH_TNT_T using_time )
+{
+#ifdef PRINTF
+	PRINTF( "Client[%d] - Using server for %d [seg]\n",
+									CLI_ID(clino), using_time );
+#endif
+//TODO	set_rgb_led( bsp_led_colors[clino] );
+}
+
+void
+bsp_cli_paused( rui8_t clino )
+{
+#ifdef PRINTF
+	PRINTF( "Client[%d] - Paused\n", CLI_ID(clino) );
+#endif
+//TODO	set_rgb_led( RGB_WHITE );
+}
+
+void
+bsp_cli_resumed( rui8_t clino )
+{
+#ifdef PRINTF
+	PRINTF( "Client[%d] - Resumed\n", CLI_ID(clino) );
+#endif
+}
+
+void
+bsp_cli_done( rui8_t clino )
+{
+#ifdef PRINTF
+	PRINTF( "Client[%d] - Done\n", CLI_ID(clino) );
+#endif
+//TODO	set_rgb_led( RGB_BLACK );
+}
+
+void
+bsp_svr_recall( rui8_t clino )
+{
+#ifdef PRINTF
+	PRINTF( "%s Recall a deferred request from client[%d]\n",
+									SVR_NAME, CLI_ID(clino) );
+#endif
+}
+
+void
+bsp_svr_paused(rui32_t ntot, rui32_t *ncr)
+{
+	rInt cn;
+    rui32_t *pNcr;
+#ifdef PRINTF
+	PRINTF( "Server paused | ");
+	PRINTF( "ntot = %d |", ntot );
+
+	for(pNcr = ncr, cn = 0; cn < NUM_CLIENTS; ++cn, ++pNcr )
+		PRINTF( " cli%d=%d |", cn, *pNcr );
+
+	PUTCHAR('\n');
+#endif
+}
+
+void
+bsp_svr_start( rui8_t clino )
+{
+	(void)0;
+}
+
+void
+bsp_svr_end( void )
+{
+
+}
+
+void
+bsp_svr_resume( void )
+{
+
+}
+
 /* ------------------------------ File footer ------------------------------ */
 /*  Use file_footer.txt file                                                 */
