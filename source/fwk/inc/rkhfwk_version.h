cbddba0e0569cceac83c6ed9334338ddb4d30ec5
Leandro Francucci <lf@vortexmakes.com>
2021-06-25 12:19:23 -0300

    Updated version and log of code changes

--- source/fwk/inc/rkhfwk_version.h
+++ source/fwk/inc/rkhfwk_version.h
@@ -76,9 +76,9 @@ extern "C" {
  *  \note
  *  For more additional information please read https://semver.org/.
  *
- *  \releasedate 03/20/2021
+ *  \releasedate 06/25/2021
  */
-#define RKH_VERSION_CODE            0x3300u
+#define RKH_VERSION_CODE            0x3400u
 
 /* ------------------------------- Data types ------------------------------ */
 /* -------------------------- External variables --------------------------- */
